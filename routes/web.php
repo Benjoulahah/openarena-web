2c0c951331708dc14a5ff4f8ff5f5c8259a3c891
ALemasson974 <aurelien.lemasson@groupe-esigelec.org>
2026-05-06 07:24:54 +0200

    Ajout Logique lancement partie et connexion AD

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,10 @@ switch ($page) {
     case 'traitement_scores_final':
         require_once __DIR__ . '/../app/traitement_scores_final.php';
         break;
+        
+    case 'traitement_lancement_match':
+        require_once __DIR__ . '/../app/traitement_lancement_match.php';
+        break;
 
     case 'home':
     default:

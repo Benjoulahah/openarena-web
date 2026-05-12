8ef7e8089bbcca02ac9aa1748eb3dc6b22e09af2
axel <136321188+malacacalex@users.noreply.github.com>
2026-05-12 10:48:26 +0200

    Update ad.php

--- config/ad.php
+++ config/ad.php
@@ -1,5 +1,5 @@
 <?php
-define('AD_HOST', ["ldaps://SRV-AD.openarena.local", "ldaps://SRV-AD-BACKUP.openarena.local"];
+define('AD_HOST', ["ldaps://SRV-AD.openarena.local", "ldaps://SRV-AD-BACKUP.openarena.local"]);
 define('AD_ADMIN', "Administrateur@openarena.local");
 define('AD_PASS', "WebAd123+"); 
 define('USER_OU', "OU=joueurs,OU=rouen,DC=openarena,DC=local"); 
